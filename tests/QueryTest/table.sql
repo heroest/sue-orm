82f28ba346c0a696ffe0f5e162533b29dd8d565a
zhangdonghai <zhangdonghai@angelalign.com>
2021-08-25 18:24:50 +0800

    change sql

--- tests/QueryTest/table.sql
+++ tests/QueryTest/table.sql
@@ -1,3 +1,48 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for cat
+-- ----------------------------
+DROP TABLE IF EXISTS `cat`;
+CREATE TABLE `cat`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
+  `age` tinyint(4) UNSIGNED NOT NULL DEFAULT 0,
+  `color` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
+  `owner_id` int(11) NOT NULL DEFAULT 0,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of cat
+-- ----------------------------
+INSERT INTO `cat` VALUES (1, 'chino', 7, 'white', 1, '2021-08-25 13:56:09', '2021-08-25 13:56:09');
+INSERT INTO `cat` VALUES (2, 'dandan', 6, 'orange', 99, '2021-08-25 14:05:05', '2021-08-25 14:05:05');
+INSERT INTO `cat` VALUES (3, 'wangmimi', 5, 'white', 2, '2021-08-25 14:54:17', '2021-08-25 14:54:17');
+
+-- ----------------------------
+-- Table structure for dog
+-- ----------------------------
+DROP TABLE IF EXISTS `dog`;
+CREATE TABLE `dog`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
+  `age` tinyint(4) NOT NULL DEFAULT 0,
+  `color` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
+  `owner_id` int(11) NOT NULL DEFAULT 0,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `unique_name`(`name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of dog
+-- ----------------------------
+
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
@@ -10,7 +55,7 @@ CREATE TABLE `user`  (
   `updated_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `unique_name`(`name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user
@@ -21,4 +66,4 @@ INSERT INTO `user` VALUES (3, 'ccc', 31, '2021-08-24 11:56:53', '2021-08-24 11:5
 INSERT INTO `user` VALUES (4, 'ddd', 41, '2021-08-24 11:56:53', '2021-08-24 11:56:53');
 INSERT INTO `user` VALUES (5, '99', 99, '2021-08-24 11:56:53', '2021-08-24 11:56:53');
 
-
+SET FOREIGN_KEY_CHECKS = 1;
